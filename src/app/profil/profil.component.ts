a63da8e4ac02c6569bc83e3686beb9c27ed060a0
Alicia1994 <78163847+Alicia1994@users.noreply.github.com>
2021-08-14 11:21:37 +0200

    Update profil.component.ts

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'ngx-webstorage';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { Observable, Subscription } from 'rxjs';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  currentUserUsername: string;
+  userSub : Subscription;
+  user$: Observable<Array<User>>;
+  
+
+  constructor(private userService: UserService, private authService: AuthService,  private localStorageService : LocalStorageService,) { }
 
   ngOnInit(): void {
+
+    this.currentUserUsername = this.localStorageService.retrieve("username");
+    this.user$ = this.userService.findUserByUsername(this.currentUserUsername);
+
+    
+
   }
 
 }
